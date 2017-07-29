2d1fc947faf013f51d754a9fe8302aafec2b04ea
dashdrum <dan@dashdrum.com>
2017-07-29 08:46:45 -0400

    Get title from page

--- links/forms.py
+++ links/forms.py
@@ -1,12 +1,39 @@
+import urllib
+
 from django.forms import (ModelForm, ValidationError, Textarea, CharField, Form, FileField, ChoiceField)
 
 from annoying.functions import get_object_or_None
 from snips.fields import EmptyChoiceField
+from bs4 import BeautifulSoup
 
 from .models import Link, Profile
 from .choices import IMPORT_TYPE_CHOICES
 
+def get_title(url):
+	soup = BeautifulSoup(urllib.request.urlopen(url))
+	return soup.title.string.encode('utf-8')
+
 class LinkForm(ModelForm):
+
+	title = CharField(required=False)
+
+	def clean(self):
+
+		cleaned_data = super(LinkForm, self).clean()
+
+		title = cleaned_data.get('title',None)
+		url = cleaned_data.get('url',None)
+
+		if not title:
+			if url:
+				title = get_title(url)
+
+		if not title:
+			raise ValidationError('Title is required')
+
+		cleaned_data['title'] = title
+
+		return cleaned_data
 
 	class Meta:
 		model = Link
