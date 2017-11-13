b851fb3bf2fe032c497390979d8ce1464bc952d4
dashdrum <dan@dashdrum.com>
2017-11-12 20:51:36 -0500

    Password reset

--- bulk/urls.py
+++ bulk/urls.py
@@ -1,17 +1,17 @@
 """bulk URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+	https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+	1. Add an import:  from my_app import views
+	2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+	1. Add an import:  from other_app.views import Home
+	2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+	1. Import the include() function: from django.conf.urls import url, include
+	2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
 from django.contrib import admin
@@ -22,18 +22,24 @@ from django.contrib.auth.forms import PasswordChangeForm
 from .views import IndexView
 
 urlpatterns = [
-    url(r'^a/', admin.site.urls),
-    url(r'^l/', include('links.urls')),
-    url(r'^m/', include('marketing.urls')),
+	url(r'^a/', admin.site.urls),
+	url(r'^l/', include('links.urls')),
+	url(r'^m/', include('marketing.urls')),
 
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(),name='logout'),
-    url(r'^change-password/$', auth_views.PasswordChangeView.as_view(
-            template_name='password_change_form.html',
-            success_url='/' ),
-        name='password_change'),
+	url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+	url(r'^logout/$', auth_views.LogoutView.as_view(),name='logout'),
+	url(r'^change-password/$', auth_views.PasswordChangeView.as_view(
+			template_name='password_change_form.html',
+			success_url='/' ),
+		name='password_change'),
 
-    url(r'^$',IndexView.as_view(),name='index'),
+	url(r'^password_reset/$', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+	url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+	url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+		auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+	url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+
+	url(r'^$',IndexView.as_view(),name='index'),
 ]
 
 ###  Django Debug Toolbar
@@ -41,8 +47,8 @@ urlpatterns = [
 from django.conf import settings
 
 if 'debug_toolbar' in settings.INSTALLED_APPS:
-    from debug_toolbar import urls as debug_urls
-    urlpatterns.append(url(r'^__debug__',include(debug_urls)))
+	from debug_toolbar import urls as debug_urls
+	urlpatterns.append(url(r'^__debug__',include(debug_urls)))
 
 ### Heartbeat
 
@@ -50,5 +56,5 @@ if 'heartbeat' in settings.INSTALLED_APPS:
   from heartbeat.urls import urlpatterns as heartbeat_urls
 
   urlpatterns += [
-    url(r'^heartbeat/', include(heartbeat_urls))
+	url(r'^heartbeat/', include(heartbeat_urls))
   ]
