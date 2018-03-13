97ee6ab1352d336340c13a0e54059d70903c82ce
dashdrum <dan@dashdrum.com>
2018-03-12 21:31:14 -0400

    Switch registration to class-based views

--- profiles/views.py
+++ profiles/views.py
@@ -50,43 +50,54 @@ class ProfileUpdateView(LoginRequiredMixin, ProfileContext, UpdateView):
 
 		return initial
 
-def signup(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			user = form.save(commit=False)
-			user.is_active = False
-			user.save()
-			current_site = get_current_site(request)
-			subject = 'Activate Your MySite Account'
-			message = render_to_string('account_activation_email.html', {
-				'user': user,
-				'domain': current_site.domain,
-				'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-				'token': account_activation_token.make_token(user),
-			})
-			user.email_user(subject, message)
-			return redirect('account_activation_sent')
-	else:
-		form = SignUpForm()
-	return render(request, 'signup.html', {'form': form})
+class SignupView(FormView):
+	form_class = SignUpForm
+	template_name = 'signup.html'
 
-def account_activation_sent(request):
-	return render(request, 'account_activation_sent.html')
-
-
-def activate(request, uidb64, token):
-	try:
-		uid = force_text(urlsafe_base64_decode(uidb64))
-		user = User.objects.get(pk=uid)
-	except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-		user = None
-
-	if user is not None and account_activation_token.check_token(user, token):
-		user.is_active = True
-		user.profile.email_confirmed = True
+	def form_valid(self,form):
+		user = form.save(commit=False)
+		user.is_active = False
 		user.save()
-		login(request, user)
-		return redirect('index')
-	else:
-		return render(request, 'account_activation_invalid.html')
+
+		current_site = get_current_site(self.request)
+		subject = 'Activate Your Bulkmarks Account'
+		message = render_to_string('account_activation_email.html', {
+			'user': user,
+			'domain': current_site.domain,
+			'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+			'token': account_activation_token.make_token(user),
+		})
+		user.email_user(subject, message)
+
+		return super(SignupView,self).form_valid(form)
+
+	def get_success_url(self):
+		return reverse('account_activation_sent')
+
+class AccountActivationSent(TemplateView):
+	template_name = 'account_activation_sent.html'
+
+class ActivateView(TemplateView):
+	template_name = 'account_activation_invalid.html'
+
+	def get(self,request,*args,**kwargs):
+		uidb64 = self.kwargs.get('uidb64',None)
+		token = self.kwargs.get('token',None)
+		try:
+			uid = force_text(urlsafe_base64_decode(uidb64))
+			user = User.objects.get(pk=uid)
+		except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+			user = None
+
+		if user is not None and account_activation_token.check_token(user, token):
+			user.is_active = True
+			user.profile.email_confirmed = True
+			user.save()
+			login(request, user)
+			return redirect('index')
+		else:
+			return super(ActivateView,self).get(request,*args,**kwargs)
+
+
+
+
